2c07cd3bdaa01b6d49a5f73c5510696c0c55db05
Nathan <natethepcspecialist@gmail.com>
2015-11-08 19:21:20 -0500

    duapte

--- server/lib/gameServer.js
+++ server/lib/gameServer.js
@@ -1,28 +1,28 @@
-var PLAYERS_ALOUD = process.argv.pop();
-var PORT = process.argv.pop();
+// var PLAYERS_ALOUD = process.argv.pop();
+// var PORT = process.argv.pop();
 var express = require('express'),
 	app = express(),
-	cookieParser = require('cookie-parser'),
-	requirejs = require('requirejs'),
+	// cookieParser = require('cookie-parser'),
+	// requirejs = require('requirejs'),
 	server = app.listen(PORT, function  () {
 		// This log is used to tell instance the server is ready
 		console.log('started'); // DO NOT DELETE
 		console.log('Game Server started on port: ', PORT);
-	}),
-	_ = require('underscore'),
+	});
+	// _ = require('underscore'),
 	// io = require('socket.io').listen(server),
-	mongoose = require('mongoose'),
-	socketCookieParser = require('socket.io-cookie-parser'),
-	bodyParser = require('body-parser');
+	// mongoose = require('mongoose'),
+	// socketCookieParser = require('socket.io-cookie-parser'),
+	// bodyParser = require('body-parser');
 
 
-app.use(express.static(process.cwd() + '/client/assets'));
-app.use(express.static(process.cwd() + '/main'));
-app.use(bodyParser.urlencoded({extended : true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.set('view engine', 'jade');
-app.set('views', process.cwd() + '/client');
+// app.use(express.static(process.cwd() + '/client/assets'));
+// app.use(express.static(process.cwd() + '/main'));
+// app.use(bodyParser.urlencoded({extended : true}));
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+// app.set('view engine', 'jade');
+// app.set('views', process.cwd() + '/client');
 
 
 // global.define = require('amdefine')(module);
@@ -46,28 +46,30 @@ app.set('views', process.cwd() + '/client');
 // Main
 // require('./main')(requirejs, io, db, PORT, PLAYERS_ALOUD);
 
-
-app.get('/', function (req, res) {
-	res.render('views/game/index');
+app.get('/', function (req ,res) {
+	res.send('hi');
 });
-app.get('/home', function (req, res) {
-	res.render('views/game/home');
-});
-app.get('/account', function (req, res) {
-	res.render('views/game/account');
-});
-app.get('/match-making', function (req, res) {
-	res.render('views/game/matchmaking');
-});
-app.get('/game-stats', function (req, res) {
-	res.render('views/game/gameStats');
-});
-app.get('/viewport', function (req, res) {
-	res.render('views/game/viewport');
-});
-app.get('/character-select', function (req, res) {
-	res.render('views/game/characterSelect');
-});
-app.get('/test', function (req, res) {
-	res.send(true);
-});
+// app.get('/', function (req, res) {
+// 	res.render('views/game/index');
+// });
+// app.get('/home', function (req, res) {
+// 	res.render('views/game/home');
+// });
+// app.get('/account', function (req, res) {
+// 	res.render('views/game/account');
+// });
+// app.get('/match-making', function (req, res) {
+// 	res.render('views/game/matchmaking');
+// });
+// app.get('/game-stats', function (req, res) {
+// 	res.render('views/game/gameStats');
+// });
+// app.get('/viewport', function (req, res) {
+// 	res.render('views/game/viewport');
+// });
+// app.get('/character-select', function (req, res) {
+// 	res.render('views/game/characterSelect');
+// });
+// app.get('/test', function (req, res) {
+// 	res.send(true);
+// });
