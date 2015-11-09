747b88f9ea0c1830d738fe9e8ed5f1bb04de6d29
Nathan <natethepcspecialist@gmail.com>
2015-11-08 20:15:35 -0500

    update

--- test.js
+++ test.js
@@ -5,7 +5,10 @@ var server = app.listen(3000);
 var io = require('socket.io').listen(server);
 
 app.get('/', function (req, res) {
-	res.send('<iframe src="http://ec2-54-165-181-175.compute-1.amazonaws.com:3001"></iframe>');
+	res.send('<iframe src="http://ec2-54-165-181-175.compute-1.amazonaws.com:3001"></iframe>'+
+		'<script src="/socket.io/socket.io.js"></script>'+
+		'<script>var io = io.connect(); io.on("hi", function(){ console.log("connected")});'+
+		);
 });
 
 io.on('connection', function (socket) {
