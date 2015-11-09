f0a5e75e381e4f7b5eafb00f184925b6b43c3436
Nathan <natethepcspecialist@gmail.com>
2015-11-08 20:13:57 -0500

    update

--- test.js
+++ test.js
@@ -1,12 +1,8 @@
 var app = require('express')();
 var spawn = require('child_process').spawn;
 var p = spawn('node', [process.cwd() + '/test2.js']);
-var io = require('socket.io');
-
-io.listen(app.listen(3000));
-process.on('uncaughtException', function (err) {
-	console.log(err);
-})
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
 
 app.get('/', function (req, res) {
 	res.send('<iframe src="http://ec2-54-165-181-175.compute-1.amazonaws.com:3001"></iframe>');
