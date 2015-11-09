28716f44e671487949d2b1fa6a1bfa2e59e31685
Nathan <natethepcspecialist@gmail.com>
2015-11-08 19:43:55 -0500

    update

--- test.js
+++ test.js
@@ -4,8 +4,9 @@ var p = spawn('node', [process.cwd() + '/test2.js']);
 process.on('uncaughtException', function (err) {
 	console.log(err);
 })
-console.log(p);
 
 app.get('/', function (req, res) {
 	res.send('<a href="http://ec2-54-165-181-175.compute-1.amazonaws.com:3001">page</a>');
 });
+
+app.listen(3000);
