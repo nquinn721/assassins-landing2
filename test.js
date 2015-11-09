1add072bf6664bc6f2a033787c80b84e6a13c4a1
Nathan <natethepcspecialist@gmail.com>
2015-11-08 19:09:03 -0500

    duapte

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 var spawn = require('child_process').spawn;
 var p = spawn('node', [process.cwd() + '/server/lib/gameServer.js']);
+process.on('uncaughtException', function (err) {
+	console.log(err);
+})
 console.log(p);
