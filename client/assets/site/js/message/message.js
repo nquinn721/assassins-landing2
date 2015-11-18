181bf2db9bf407b9c58ac2e2fdd0a9f003124f4c
Nathan <natethepcspecialist@gmail.com>
2015-11-17 23:44:22 -0500

    'update'

--- client/assets/site/js/message/message.js
+++ client/assets/site/js/message/message.js
@@ -1,13 +1,28 @@
-app.factory('message', ['$rootScope', function ($rootScope) {
+app.factory('message', ['$rootScope', '$sce', function ($rootScope, $sce) {
 	
 	return {
+		confirm : function (msg, cb, messageType) {
+			this.show(msg, messageType || 1);	
+			this.cb = cb;
+		},
 		show : function (msg, messageType) {
+			$rootScope.showHideMessageSplash = 'fadeIn';
+			$rootScope.showHideMessageBox = 'bounceInDown';
 			$rootScope.showMessage = true;
-			$rootScope.message = msg || 'Something went wrong';
+			$rootScope.message = $sce.trustAsHtml(msg) || 'Something went wrong';
 			$rootScope.messageType = messageType === 0 ? 'ok' : messageType === 1 ? 'okcancel' : 'yesno';
 		},
 		hide : function () {
-			$rootScope.showMessage = false;
+			$rootScope.showHideMessageSplash = 'fadeOut';
+			$rootScope.showHideMessageBox = 'bounceOutUp';
+			setTimeout(function () {
+				$rootScope.showMessage = false;
+				$rootScope.$apply();
+			}, 500);
+		},
+		confirmMessage : function () {
+			this.cb();
+			this.hide();
 		}
 	}
 }]);
--- client/assets/site/js/message/messageCtrl.js
+++ client/assets/site/js/message/messageCtrl.js
@@ -2,4 +2,7 @@ app.controller('messageCtrl', ['message', function (message) {
 	this.hideMessage = function () {
 		message.hide();
 	}
+	this.confirmMessage = function () {
+		message.confirmMessage();
+	}
 }]);
