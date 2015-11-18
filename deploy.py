3f64c22bd3bef7c3b98e67d2f5fb242fc5011986
Nathan <natethepcspecialist@gmail.com>
2015-11-17 22:52:58 -0500

    update

--- deploy.py
+++ deploy.py
@@ -3,5 +3,4 @@ import os
 os.system("git add . " +
 	"&& git commit -m 'update'" +
 	"&& git push origin master" +
-	"&& ssh -i al.pem ubuntu@ec2-54-165-181-175.compute-1.amazonaws.com 'python deploy.py'" +
- 	"&& cd /Applications/XAMPP/htdocs/assassins-landing2")
+	"&& ssh -i al.pem ubuntu@ec2-54-165-181-175.compute-1.amazonaws.com 'python deploy.py'")
