a6e3c5dc65426b0e000add5158dc850cb04d4030
Nathan <natethepcspecialist@gmail.com>
2015-11-17 23:55:05 -0500

    'update'

--- server/chat/chat-routes.js
+++ server/chat/chat-routes.js
@@ -7,6 +7,7 @@ module.exports = function (app, db, io) {
 
 		db.getSessionAndAccountByUserId(to, function (acc, session) {
 			var room = '/chat-' + from + '-' + to;
+			console.log(session);
 			if(session)
 				if(session.socketId)
 					io.sockets.connected[session.socketId].emit('startChat', room);
