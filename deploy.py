627325735126aaf07ee933b2161e30d5c547f4c7
Nathan <natethepcspecialist@gmail.com>
2015-11-17 23:05:04 -0500

    'update'

--- deploy.py
+++ deploy.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
-os.system("git add . " +
+os.system("git add -u " +
 	"&& git commit -m 'update'" +
 	"&& git push origin master" +
 	"&& ssh -i al.pem ubuntu@ec2-54-165-181-175.compute-1.amazonaws.com 'python deploy.py'")
+ # https://nquinn721:Adammike721@github.com/nquinn721/assassins-landing2.git
